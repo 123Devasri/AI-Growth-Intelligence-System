af72ff282800d653eed2e45c8748ed458d974b35
123Devasri <rdevasri123@gmail.com>
2026-06-03 21:28:34 +0530

    RESTFulAPI

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 app = Flask(__name__) #It creates the Flask application object and uses __name__ to help Flask locate resources such as templates and static files.
+students = [
+    {
+        "name":"Deva",
+        "age":"20"
+    }
+]
 @app.route("/")
  #It maps a URL endpoint to a Python function. When a client requests that URL, Flask executes the associated function and returns its response.
  #This is called a decorator. Flask internally does something similar to: app.route("/")(home)
  #Register home() for the URL "/"
 def home():
     return "Hello from API"
-@app.route("/about")
-def about():
-    return "about page"
+@app.route("/students")
+def get_students():
+    return students
+#does not work in browsers
+@app.route("/students",methods=["POST"])
+def create_students():
+    data=request.json
+    students.append(data)
+    return students
 app.run(debug=True) #It starts the Flask development server and enables automatic reloading and detailed error messages.
