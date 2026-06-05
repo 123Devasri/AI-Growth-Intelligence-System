18df316dced06a16d0ece22cf49264b3f8c689bc
123Devasri <rdevasri123@gmail.com>
2026-06-05 19:19:49 +0530

    Updated CRUD Application

--- app.py
+++ app.py
@@ -13,9 +13,11 @@ students = [
  #Register home() for the URL "/"
 def home():
     return "Hello from API"
+
 @app.route("/students",methods=["GET"])
 def get_students():
     return students,200
+
 @app.route("/students/<id:int>",methods=["GET"])
 def get_one_student(id):
     for student in students:
@@ -28,5 +30,22 @@ def get_one_student(id):
 def create_students():
     data=request.json
     students.append(data)
-    return students
+    return students,201
+
+@app.route("/students/<id:int>",methods=["PUT"])
+def update_student(id):
+    data=request.json
+    for studnent in students:
+        if student["id"]==id:
+            student["name"]=data["name"]
+            return student,200
+    return {"Error;Student not found"},404
+
+@app.route("/students/<id:int>",methods=["DELETE"])
+def remove_student(id):
+    for student in students:
+        if student["id"]==id:
+            student.remove(student)
+            return {"message:student removed successfully"},200
+    retrun {"error:student not found"},404
 app.run(debug=True) #It starts the Flask development server and enables automatic reloading and detailed error messages.
